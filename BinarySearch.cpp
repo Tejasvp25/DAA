7ffce8ef037bd1e84f61bd508d016409120c5c19
Tejasvp25 <tejasvp25@gmail.com>
2021-09-29 21:07:30 +0530

    Binary Search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,39 +1,61 @@
 /**
  *  Name - Tejas Patil
- *  GR No - xxxxxxxx
- *  Roll No - xxxxxx
+ *  GR No - 22020163
+ *  Roll No - 323080
  *  Div - TY-C (C3 Batch) 
  *
  *  @author Tejas-PC
+ * 
+ *  https://github.com/Tejasvp25/DAA/blob/master/BinarySearch.cpp
  **/
 
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int x, int n, int low, int high)
+int binarySearch(int arr[], int x, int low, int high)
 {
-    while (low <= high)
+    int mid;
+    if (low > high)
+        return -1;
+    else
     {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == x)
+        mid = (low + high) / 2;
+        if (x == arr[mid])
             return mid;
-        else if (arr[mid] < x)
-            low = mid + 1;
+        else if (x > arr[mid])
+            return binarySearch(arr, x, mid + 1, high);
         else
-            high = mid - 1;
+            return binarySearch(arr, x, low, mid - 1);
     }
-    return -1;
 }
 
 int main()
 {
-    int arr[] = {3, 5, 7, 9, 10, 56, 78, 97, 101};
-    int x = 56;
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int size, key;
+    cout << "Enter Number of Elements in Array : ";
+    cin >> size;
+    int arr[size];
 
-    int result = binarySearch(arr, x, n, 0, n - 1);
+    cout << "Enter Elements of Array : ";
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+
+    cout << "Enter Key to Find : ";
+    cin >> key;
+
+    int result = binarySearch(arr, key, 0, size - 1);
     if (result == -1)
         cout << "Element Not found:" << endl;
     else
         cout << "Element foound at location " << result << endl;
 }
+
+/**
+ * 
+ * Output:
+ * 
+ * Enter Number of Elements in Array : 6
+ * Enter Elements of Array : 1 7 8 11 33 55
+ * Enter Key to Find : 7
+ * Element foound at location 1
+*/
